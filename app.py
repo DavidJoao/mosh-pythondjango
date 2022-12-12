fe09167b26bbe68593af3a645d429934f332a921
DavidJoao <davidsandoval596@gmail.com>
2022-12-12 11:59:32 -0800

    While loops

--- app.py
+++ app.py
@@ -9,24 +9,24 @@ patient_name = 'John Smith'
 age = 20
 new_patient = True
 
-# the_name = input('What is your name? \n')
-# fav_color = input('What is your favorite color? \n')
-# print('Hi ' + the_name + ', I see your favorite color is ' + fav_color)
+the_name = input('What is your name? \n')
+fav_color = input('What is your favorite color? \n')
+print('Hi ' + the_name + ', I see your favorite color is ' + fav_color)
 
-#Type conversion
-# birth_year = input('Birth year: \n')
-# age = 2020 - int(birth_year)
-# print(age)
-# pounds = input('Enter your weight in pounds: \n')
-# kilograms = float(pounds) * 0.45
-# print ('Your weight in kilograms is ' + str(kilograms))
+# Type conversion
+birth_year = input('Birth year: \n')
+age = 2020 - int(birth_year)
+print(age)
+pounds = input('Enter your weight in pounds: \n')
+kilograms = float(pounds) * 0.45
+print ('Your weight in kilograms is ' + str(kilograms))
 
-##String methods
-# course = 'Python for beginners'
-# print(len(course)) #Prints length of string
-# course.upper() #Makes string uppercase, doesnt modify original string
-# course.find('f') #finds index of character
-# 'Python' in course #returns boolean 
+#String methods
+course = 'Python for beginners'
+print(len(course)) #Prints length of string
+course.upper() #Makes string uppercase, doesnt modify original string
+course.find('f') #finds index of character
+'Python' in course #returns boolean 
 
 x = 2.9
 round(x) #rounds up to 3
@@ -77,3 +77,62 @@ elif len(name) > 50:
     print('Maximum characters exceeded')
 else:
     print('Name looks good')
+
+#While loops
+#while condition: run loop
+#Example:
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+#Guessing game using While Loop
+secret_number = 6
+guess_limit = 3
+guess_count = 0
+while guess_count < guess_limit:
+    guess = int(input('Guess the correct number between 0 and 10 \n'))
+    guess_count += 1
+    if guess == secret_number:
+        print('Correct number!')
+        break
+    else: 
+        print('Wrong number')
+else:
+    print('Sorry, you failed :(')
+
+#Car exercise
+answer = ''
+is_started = False
+print('start - to start the car')
+print('stop - to stop the car')
+print('quit - to exit')
+print('help - to see commands')
+while answer != 'quit':
+    answer = input('Enter command: \n')
+    if answer == 'start':
+        if is_started == False:
+            is_started = True
+            print('Car started!')
+        elif is_started == True:
+            print('Car already started!')
+    elif answer == 'stop':
+        if is_started == False:
+            print('Already stopped!')
+        elif is_started == True:
+            is_started = False
+            print('Car stopped!')
+    elif answer == 'quit':
+        break
+    elif answer == 'help':
+        print("""
+        start - to start the car
+        stop - to stop the car
+        quit - to exit
+        help - to see commands
+        """)
+    else:
+        print("Sorry, I don't understand that command")
+    
+
