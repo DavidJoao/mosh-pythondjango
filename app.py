cbf2ec8a2ec7f6988d73020fcfd140772c982aab
DavidJoao <davidsandoval596@gmail.com>
2022-12-11 21:30:27 -0800

    Math module

--- app.py
+++ app.py
@@ -9,14 +9,30 @@ patient_name = 'John Smith'
 age = 20
 new_patient = True
 
-# the_name = input('What is your name? \n')
-# fav_color = input('What is your favorite color? \n')
-# print('Hi ' + the_name + ', I see your favorite color is ' + fav_color)
+the_name = input('What is your name? \n')
+fav_color = input('What is your favorite color? \n')
+print('Hi ' + the_name + ', I see your favorite color is ' + fav_color)
 
 #Type conversion
-# birth_year = input('Birth year: \n')
-# age = 2020 - int(birth_year)
-# print(age)
+birth_year = input('Birth year: \n')
+age = 2020 - int(birth_year)
+print(age)
 pounds = input('Enter your weight in pounds: \n')
 kilograms = float(pounds) * 0.45
 print ('Your weight in kilograms is ' + str(kilograms))
+
+##String methods
+course = 'Python for beginners'
+print(len(course)) #Prints length of string
+course.upper() #Makes string uppercase, doesnt modify original string
+course.find('f') #finds index of character
+'Python' in course #returns boolean 
+
+x = 2.9
+round(x) #rounds up to 3
+abs(x) #returns always positive
+#there also exist ceil and floor methods
+
+#If statements
+
+
