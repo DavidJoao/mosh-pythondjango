34b643d249292d30f17678d4a54f19b39d534328
DavidJoao <davidsandoval596@gmail.com>
2022-12-12 19:39:11 -0800

    Created and re-ordered files and code

--- app.py
+++ app.py
@@ -66,157 +66,16 @@ has_good_credit = False
 #They are useful for comparing variables with value, for example comparing a variable 'temperature' > 30
 #Different comparison operators are >, <, <=, >=, ==, !=
 
-# temperature = 30
-# if temperature > 30:
-#     print("It's a hot day")
-# else:
-#     print("It's not a hot day")
+temperature = 30
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
 
-# name = 'David'
-# if len(name) < 3: 
-#     print('Name needs to contain more than 3 characters')
-# elif len(name) > 50:
-#     print('Maximum characters exceeded')
-# else:
-#     print('Name looks good')
-
-
-# #########################################      While loops
-#while condition: run loop
-#Example:
-
-# i = 1
-# while i <= 5:
-#     print(i)
-#     i += 1
-
-# #Guessing game using While Loop
-
-# secret_number = 6
-# guess_limit = 3
-# guess_count = 0
-# while guess_count < guess_limit:
-#     guess = int(input('Guess the correct number between 0 and 10 \n'))
-#     guess_count += 1
-#     if guess == secret_number:
-#         print('Correct number!')
-#         break
-#     else: 
-#         print('Wrong number')
-# else:
-#     print('Sorry, you failed :(')
-
-#Car exercise
-
-# answer = ''
-# is_started = False
-# print('start - to start the car')
-# print('stop - to stop the car')
-# print('quit - to exit')
-# print('help - to see commands')
-# while answer != 'quit':
-#     answer = input('Enter command: \n')
-#     if answer == 'start':
-#         if is_started == False:
-#             is_started = True
-#             print('Car started!')
-#         elif is_started == True:
-#             print('Car already started!')
-#     elif answer == 'stop':
-#         if is_started == False:
-#             print('Already stopped!')
-#         elif is_started == True:
-#             is_started = False
-#             print('Car stopped!')
-#     elif answer == 'quit':
-#         break
-#     elif answer == 'help':
-#         print("""
-#         start - to start the car
-#         stop - to stop the car
-#         quit - to exit
-#         help - to see commands
-#         """)
-#     else:
-#         print("Sorry, I don't understand that command")
-    
-#########################################      For Loops
-
-example = 'Python'
-for item in example:
-        if item == 'o':
-            item = 'x'
-        print(item)
-# or
-for item in range(10):
-    print(item)
-
-#if we want to set a specific range we can do something like this:
-for item in range (7, 10): #we can add a third argument to modify how many steps are taken by iteration
-    print(item)
-
-prices = [10, 20, 30]
-total = 0
-for item in prices:
-    total += item
-print(f"The total is {total}")
-
-#########################################      Nested Loops
-
-# for x in range(4):
-#     for y in range (3):
-#         print(f"({x}, {y})")
-
-numbers = [2, 2, 2, 2, 5]
-
-for x_count in numbers:
-    output = ''
-    for count in range(x_count):
-        output += 'x'
-    print(output)
-
-#########################################      Lists
-
-names = ['John', 'Bob', 'David', 'Sarah']
-
-print(names)
-
-another_list = [1, 7, 15, 1, 7, 1, 5]
-max = another_list[0]
-for number in another_list:
-    if number > max:
-        max = number
-print(max)
-
-#########################################      Two Dimensional Lists
-
-#Example of a 3x3 Matrix
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-matrix_max = matrix[0][0]
-for eachlist in matrix:
-    for eachnumber in eachlist:
-        if eachnumber > matrix_max:
-            matrix_max = eachnumber
-print(matrix_max)
-
-#########################################      Lists Methods or Functions
-
-new_list = [5, 1, 6, 5, 10, 8, 10, 26]
-new_list.append(20) #inserts item at the end of list
-new_list.insert(2, 3) #inserts item in specific index of list, first argument represents index and second argument represents the item inserted
-print(new_list)
-uniques = []
-for number in new_list:
-    if number not in uniques:
-        uniques.append(number)
-print(uniques)
-    
-
-
-
-
+name = 'David'
+if len(name) < 3: 
+    print('Name needs to contain more than 3 characters')
+elif len(name) > 50:
+    print('Maximum characters exceeded')
+else:
+    print('Name looks good')
