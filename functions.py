337d5c29bf72acc857e24cead00e2cd20148694d
DavidJoao <davidsandoval596@gmail.com>
2022-12-14 19:45:45 -0800

    Reviewed return statements and exceptions

--- functions.py
+++ functions.py
@@ -19,3 +19,36 @@ greet_person('David', 'Sandoval')
 #Keyword arguments are used to make code more readable when calling functions, for example:
 greet_person(name="David", last_name="Sandoval") #Output will be the same, but now we know what arguments represent each String
 
+
+
+#########################################      Return Statements
+def square(number):
+    return number * number
+#returns a value, so we can assign it to a variable
+square_of_three = square(3)
+print(square_of_three)
+
+
+#########################################      Creating Re-usable functions
+def converter(message):
+    message_array = message.split(" ")
+    final_message = ''
+    emojis = {
+        ":)" : "😀",
+        ":(": "😞",
+        "tinker": "🐶",
+        "que": "🧀"
+    }
+    for word in message_array:
+        final_message += emojis.get(word, word) + " "
+    return final_message
+
+
+print(converter('Nooo tinker que :('))
+
+#########################################      Exceptions
+try:
+    age = int(input('Age: \n'))
+    print(age)
+except ValueError:
+    print('Invalid value')
